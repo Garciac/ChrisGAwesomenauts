a44688539e8d579f7bedd554fca4f07e21da0177
Chris Garcia <chrisg1245@hotmail.com>
2015-04-16 14:03:04 -0700

    no message

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -5,11 +5,11 @@ game.SpendExp = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
 	
-                me.input.bindKEY(me.input.KEY.F1, "F1");
-                me.input.bindKEY(me.input.KEY.F2, "F2");
-                me.input.bindKEY(me.input.KEY.F3, "F3");
-                me.input.bindKEY(me.input.KEY.F4, "F4");
-                me.input.bindKEY(me.input.KEY.F5, "F5");              
+                me.input.bindKey(me.input.KEY.F1, "F1");
+                me.input.bindKey(me.input.KEY.F2, "F2");
+                me.input.bindKey(me.input.KEY.F3, "F3");
+                me.input.bindKey(me.input.KEY.F4, "F4");
+                me.input.bindKey(me.input.KEY.F5, "F5");              
                 var exp1cost = ((game.data.exp1 + 1)* 10);
                 
                 me.game.world.addChild(new(me.Renderable.extend({
@@ -56,11 +56,11 @@ game.SpendExp = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKEY(me.input.KEY.F1, "F1");
-                me.input.unbindKEY(me.input.KEY.F2, "F2");
-                me.input.unbindKEY(me.input.KEY.F3, "F3");
-                me.input.unbindKEY(me.input.KEY.F4, "F4");
-                me.input.unbindKEY(me.input.KEY.F5, "F5");   
+		me.input.unbindKey(me.input.KEY.F1, "F1");
+                me.input.unbindKey(me.input.KEY.F2, "F2");
+                me.input.unbindKey(me.input.KEY.F3, "F3");
+                me.input.unbindKey(me.input.KEY.F4, "F4");
+                me.input.unbindKey(me.input.KEY.F5, "F5");   
                 me.event.unsubscribe(this.handler);
 	}
 });
