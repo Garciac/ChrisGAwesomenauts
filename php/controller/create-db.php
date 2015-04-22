4bd7127d9cb2c6dcceee2cebf07822111ce482f9
Chris Garcia <chrisg1245@hotmail.com>
2015-04-22 13:57:00 -0700

    no message

--- php/controller/create-db.php
+++ php/controller/create-db.php
@@ -16,9 +16,3 @@ $query = $_SESSION["connection"]->query("CREATE TABLE users ("
         . "exp3 int(4),"
         . "exp4 int(4),"
         . "PRIMARY KEY (id))");
-
-if ($query) {
-    echo "<p>Sucessfully created table: users</p>";
-} else {
-    echo "<p>" . $_SESSION["connection"]->error . "</p>";
-}
